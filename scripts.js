9de4c99c0ee08b9498318978283334809030112c
oskar <67971701+k4rdel@users.noreply.github.com>
2022-05-24 22:18:26 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -40,3 +40,26 @@ $( function() {
     change: ModyfikacjaZarowki
   });
 });
+
+//telefon
+function detectMob() {
+    const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
+    ];
+    
+    return toMatch.some((toMatchItem) => {
+        return navigator.userAgent.match(toMatchItem);
+    });
+}
+
+if(detectMob() == true) {
+    while(true){
+      alert("Niestety ta strona nie jest dostępna na urządzeniach mobilnych.");
+    }
+}
